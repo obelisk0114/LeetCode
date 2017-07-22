a9a442936e71c1ea51109bdf477f89458049a958
obelisk0114 <obelisk0114@yahoo.com.tw>
2017-07-21 23:06:10 -0500

    Add OJ287
    287. Find the Duplicate Number

---
+++ src/OJ0281_0290/Find_the_Duplicate_Number.java
@@ -0,0 +1,81 @@
+package OJ0281_0290;
+
+/*
+ * http://www.csie.ntnu.edu.tw/~u91029/Function.html#2
+ */
+
+public class Find_the_Duplicate_Number {
+	/*
+	 * https://discuss.leetcode.com/topic/25685/java-o-n-time-and-o-1-space-solution-similar-to-find-loop-in-linkedlist
+	 * 
+	 * Rf : 
+	 * https://discuss.leetcode.com/topic/26481/tortoise-haire-cycle-detection-algorithm
+	 * https://discuss.leetcode.com/topic/25601/a-java-solution-o-n-time-and-o-1-space
+	 * 
+	 * Leetcode 142 Linked List Cycle II
+	 */
+	public int findDuplicate(int[] nums) {
+		int n = nums.length;
+		int slow = n;
+		int fast = n;
+		do {
+			slow = nums[slow - 1];
+			fast = nums[nums[fast - 1] - 1];
+		} while (slow != fast);
+		slow = n;
+		while (slow != fast) {
+			slow = nums[slow - 1];
+			fast = nums[fast - 1];
+		}
+		return slow;
+	}
+	
+	/*
+	 * https://discuss.leetcode.com/topic/27420/java-o-1-space-using-binary-search
+	 * 
+	 * Rf : https://discuss.leetcode.com/topic/25580/two-solutions-with-explanation-o-nlog-n-and-o-n-time-o-1-space-without-changing-the-input-array
+	 */
+	public int findDuplicate_binarySearch(int[] nums) {
+		int low = 1, high = nums.length - 1;
+		while (low <= high) {
+			int mid = (int) (low + (high - low) * 0.5);
+			int cnt = 0;
+			for (int a : nums) {
+				if (a <= mid)
+					++cnt;
+			}
+			if (cnt <= mid)
+				low = mid + 1;
+			else
+				high = mid - 1;
+		}
+		return low;
+	}
+	
+	/*
+	 * https://discuss.leetcode.com/topic/38493/o-32-n-solution-using-bit-manipulation-in-10-lines
+	 * 
+	 * We can count the sum of each 32 bits separately for the given array 
+	 * (stored in "b" variable) and for the array [1, 2, 3, ..., n] 
+	 * (stored in "a" variable). 
+	 * If "b" is greater than "a", 
+	 * it means that duplicated number has 1 at the current bit position 
+	 * (otherwise, "b" couldn't be greater than "a").
+	 */
+	public int findDuplicate_bit(int[] nums) {
+		int n = nums.length - 1, res = 0;
+		for (int p = 0; p < 32; ++p) {
+			int bit = (1 << p), a = 0, b = 0;
+			for (int i = 0; i <= n; ++i) {
+				if ( (i & bit) > 0)
+					++a;
+				if ((nums[i] & bit) > 0)
+					++b;
+			}
+			if (b > a)
+				res += bit;
+		}
+		return res;
+	}
+
+}
