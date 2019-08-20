be4f1255a7391fe9df5cbf5b368d440b40671610
obelisk0114 <obelisk0114@yahoo.com.tw>
2019-08-20 14:53:50 -0700

    Update OJ5
    5. Longest Palindromic Substring

--- src/OJ0001_0010/Longest_Palindromic_Substring.java
+++ src/OJ0001_0010/Longest_Palindromic_Substring.java
@@ -1,98 +1,222 @@
 package OJ0001_0010;
 
-/*
- * http://www.csie.ntnu.edu.tw/~u91029/Palindrome.html
- * 
- * Use Manacher's Algorithm 
- * Last = L = j ; RightMost = R = j+z(j)-1 ; Gusfield's Algorithm
- * nL = n = z[i]
- * length of palindrome that we use in the algorithm = z[i]-1
- * 
- */
-
-import java.util.Scanner;
-
 public class Longest_Palindromic_Substring {
-	private static int[] z;
-	private char[] s;         // Insert another character which is not in the string 
-	
-	public Longest_Palindromic_Substring(char[] line) {
-		int n = line.length;
-		z = new int[2 * n + 1];
-		z[0] = 1;
-		z[2 * n] = 1;
-		s = new char[2 * n + 1];
-		for (int i = 0; i < 2 * n + 1; i++) {
-			if (i % 2 == 0)
-				s[i] = '.';
-			else
-				s[i] = line[i / 2];
-		}
-	}
-	
-	private int match(int a, int b) {
-		int i = 0;
-		int n = s.length;
-		while (a-i >= 0 && b+i < n && s[a-i] == s[b+i])
-			i++;
-		return i;
-	}
-	
-	void find(int n) {
-		int Last = 0;
-		int RightMost = 0;
-		for (int i = 1; i < 2 * n; i++) {
-			int ii = Last - (i - Last);
-			int nL = RightMost + 1 - i;
-			
-			if (i > RightMost) {
-				z[i] = match(i, i);
-				Last = i;
-				RightMost = i + z[i] - 1;
-			}
-			else if (z[ii] == nL) {
-				z[i] = nL + match(i - nL, i + nL);
-				Last = i;
-				RightMost = i + z[i] - 1;
-			}
-			else {
-				z[i] = Math.min(z[ii], nL);
-			}
-		}
-	}
-	
-	int longestPosition() {
-		int maxPosition = 0;
-		for (int i = 1; i < z.length; i++) {
-			if (z[i] > z[maxPosition])
-				maxPosition = i;
-		}
-		return maxPosition;
-	}
-	
-	void printLongest(int p) {
-		int n = z[p];
-		for (int i = 0; i < 2 * n - 1; i++) {
-			if ( i % 2 == 1)
-				System.out.print(s[p - n + 1 + i]);
-		}
-	}
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner keyboard = new Scanner(System.in);
-		System.out.println("This is Longest_Palindromic_Substring.\nInput the String.");
-		String line = keyboard.nextLine();
-		int length = line.length();
-		char[] ch = line.toCharArray();
-		Longest_Palindromic_Substring lps = new Longest_Palindromic_Substring(ch);
-		lps.find(length);
-		int longest = (z[lps.longestPosition()] - 1) / 2;
-		System.out.println("length of Longest Palindromic Substring : " + longest);
-		lps.printLongest(lps.longestPosition());
+	/*
+	 * The following 2 functions are from this link.
+	 * https://leetcode.com/articles/longest-palindromic-substring/
+	 * 
+	 * A palindrome can be expanded from its center, and there are only 2n - 1 centers.
+	 * The reason is the center of a palindrome can be in between two letters. Such 
+	 * palindromes have even number of letters (such as "abba") and its center are 
+	 * between the two 'b's.
+	 * 
+	 * Rf : https://leetcode.com/problems/longest-palindromic-substring/discuss/2928/Very-simple-clean-java-solution
+	 */
+	public String longestPalindrome(String s) {
+		if (s == null || s.length() < 1)
+			return "";
 		
-		keyboard.close();
-
+		int start = 0, end = 0;
+		for (int i = 0; i < s.length(); i++) {
+			int len1 = expandAroundCenter(s, i, i);       // assume odd length
+			int len2 = expandAroundCenter(s, i, i + 1);   // assume even length
+			
+			int len = Math.max(len1, len2);
+			if (len > end - start) {
+				start = i - (len - 1) / 2;
+				end = i + len / 2;
+			}
+		}
+		return s.substring(start, end + 1);
 	}
 
+	private int expandAroundCenter(String s, int left, int right) {
+		int L = left, R = right;
+		while (L >= 0 && R < s.length() && s.charAt(L) == s.charAt(R)) {
+			L--;
+			R++;
+		}
+		return R - L - 1;
+	}
+	
+	/*
+	 * The following 2 functions are from this link.
+	 * https://leetcode.com/problems/longest-palindromic-substring/discuss/3060/(AC)-relatively-short-and-very-clear-Java-solution
+	 * 
+	 * Every time we move to right, we only need to consider whether using this new 
+	 * character as tail could produce new palindrome string of length 
+	 * (current length +1) or (current length +2)
+	 * 
+	 * Suppose max is the longest palindrome substring of s[0..i). Then for s[0..i+1) 
+	 * we only need to consider palindromes ending at s[i] (inclusive)
+	 * 
+	 * If we were able to find a palindrome of length max + k (k > 2) ending at i, 
+	 * then it would mean that there is a palindrome of length max + k - 2 > max 
+	 * ending at i - 1, which contradicts the assumption that max is the maximum 
+	 * length of a palindrome found so far.
+	 * 
+	 * Rf : https://leetcode.com/problems/longest-palindromic-substring/discuss/3003/Java-easy-understanding-solution.-Beats-97/3755
+	 * 
+	 * Other code :
+	 * https://leetcode.com/problems/longest-palindromic-substring/discuss/3003/Java-easy-understanding-solution.-Beats-97
+	 */
+	public String longestPalindrome_check(String s) {
+		String res = "";
+		int currLength = 0;
+		for (int i = 0; i < s.length(); i++) {
+			if (isPalindrome(s, i - currLength - 1, i)) {
+				res = s.substring(i - currLength - 1, i + 1);
+				currLength = currLength + 2;
+			} 
+			else if (isPalindrome(s, i - currLength, i)) {
+				res = s.substring(i - currLength, i + 1);
+				currLength = currLength + 1;
+			}
+		}
+		return res;
+	}
+
+	public boolean isPalindrome(String s, int begin, int end) {
+		if (begin < 0)
+			return false;
+		while (begin < end) {
+			if (s.charAt(begin++) != s.charAt(end--))
+				return false;
+		}
+		return true;
+	}
+	
+	/*
+	 * https://leetcode.com/problems/longest-palindromic-substring/discuss/110375/Java-DP-solution-with-optimization-in-space-from-O(n2)-to-O(n)-time-O(n2)-with-very-clear-explanations
+	 * 
+	 * dp(i, j) represents whether s(i ... j) can form a palindromic substring, 
+	 * dp(i, j) is true when s(i) equals to s(j) and s(i+1 ... j-1) is a palindromic 
+	 * substring. When we found a palindrome, check if it's the longest one.
+	 * 
+	 * Rf :
+	 * https://leetcode.com/problems/longest-palindromic-substring/discuss/2921/Share-my-Java-solution-using-dynamic-programming
+	 * https://leetcode.com/problems/longest-palindromic-substring/discuss/2921/Share-my-Java-solution-using-dynamic-programming/243271
+	 * https://leetcode.com/problems/longest-palindromic-substring/discuss/2921/Share-my-Java-solution-using-dynamic-programming/3567
+	 * 
+	 * Other code :
+	 * https://leetcode.com/problems/longest-palindromic-substring/discuss/151144/Bottom-up-DP-Logical-Thinking
+	 */
+	public String longestPalindrome_dp(String s) {
+		int n = s.length();
+		String result = "";
+		boolean[][] dp = new boolean[n][n];
+
+		// i goes from n - 1 to 0, j goes from i to n - 1, 
+		// to make sure i occurs before j
+		for (int i = n - 1; i >= 0; i--) {
+			for (int j = i; j < n; j++) {
+				// if you have a word whose number is less than 2, and 
+				// dp[i+1][j-1] will not success
+				dp[i][j] = s.charAt(i) == s.charAt(j) && 
+						(j - i < 2 || dp[i + 1][j - 1]);
+				
+				// if dp[i][j] is true¡Aupdate result
+				if (dp[i][j] && (result.equals("") || j - i + 1 > result.length())) {
+					result = s.substring(i, j + 1); // substring includes j
+				}
+			}
+		}
+		return result;
+	}
+	
+	// https://leetcode.com/problems/longest-palindromic-substring/discuss/128054/Java-(Beats-98)-and-JavaScript-(Beats-100)-O(n)-Manacher's-Algorithm-with-Detailed-Explanation
+	public String longestPalindrome_Manacher(String s) {
+		
+        /* Preprocess s: insert '#' between characters, so we don't need to worry about 
+         * even or odd length palindromes. */
+        char[] newStr = new char[s.length() * 2 + 1];
+        newStr[0] = '#';
+        for (int i = 0; i < s.length(); i++) {
+            newStr[2 * i + 1] = s.charAt(i);
+            newStr[2 * i + 2] = '#';
+        }
+        
+        /* Process newStr */
+        /* dp[i] is the length of LPS centered at i */
+        int[] dp = new int[newStr.length];
+        
+        /**
+         * For better understanding, here we define "friend substring", or "friend":
+         * "friend substring" has the largest end-index in all checked substrings that
+         * are palindromes. We start at friendCenter = 0 and update it in each cycles.
+         */
+        int friendCenter = 0, friendRadius = 0, lpsCenter = 0, lpsRadius = 0;
+        
+        /* j is the symmetry of i with respect to friendCenter */
+        int j;
+        for (int i = 0; i < newStr.length; i++) {
+            /* Calculate dp[i] */
+            if (friendCenter + friendRadius > i) {
+                /**
+                 * This is the most important part of the algorithm.
+                 * 
+                 * Normally we start from dp[i] = 1 and then try to expand dp[i] by 
+                 * doing brute-force palindromic checks. However, if i is in the range 
+                 * of friend (friendCenter + friendRadius > i), we can expect
+                 * dp[i] = dp[j] because friend is a palindrome. This only works within 
+                 * the range of friend, so the
+                 * max value of dp[i] we can trust is (friendEnd - i).
+                 * 
+                 * Here is an example:
+                 *
+                 *     friendStart   j             friendCenter  i     friendEnd                         
+                 *               |   |             |             |     |
+                 * String: - - d c b a b c d - - - - - - - d c b a b c ? - - - - - - - -
+                 *               [--------friend (palindrome)--------]
+                 *
+                 * In this example, (friendEnd - i) = 3, so we can only be certain that 
+                 * radius <= 3 part around i is a palindrome (i.e. "cbabc" part). We 
+                 * still need to check the character at "?".
+                 */
+                j = friendCenter - (i - friendCenter);
+                dp[i] = Math.min(dp[j], (friendCenter + friendRadius) - i);
+            }
+            else {
+                /* Calculate from scratch */
+                dp[i] = 1;
+            }
+            
+            /* Check palindrome and expand dp[i] */
+			while (i + dp[i] < newStr.length && i - dp[i] >= 0 
+					&& newStr[i + dp[i]] == newStr[i - dp[i]])
+				dp[i]++;
+
+            /* Check if i should become the new friend */
+            if (friendCenter + friendRadius < i + dp[i]) {
+                friendCenter = i;
+                friendRadius = dp[i];
+            }
+            
+            /* Update longest palindrome */
+            if (lpsRadius < dp[i]) {
+                lpsCenter = i;
+                lpsRadius = dp[i];
+            }
+        }
+        return s.substring((lpsCenter - lpsRadius + 1) / 2, 
+        		(lpsCenter + lpsRadius - 1) / 2);
+    }
+	
+	/**
+     * Python collections
+     * 
+     * https://leetcode.com/problems/longest-palindromic-substring/discuss/2954/Python-easy-to-understand-solution-with-comments-(from-middle-to-two-ends).
+     * https://leetcode.com/problems/longest-palindromic-substring/discuss/2925/Python-O(n2)-method-with-some-optimization-88ms.
+     * https://leetcode.com/problems/longest-palindromic-substring/discuss/121496/Python-DP-solution
+     * https://leetcode.com/problems/longest-palindromic-substring/discuss/3337/Manacher-algorithm-in-Python-O(n)
+     */
+    
+    /**
+     * C++ collections
+     * 
+     * https://leetcode.com/problems/longest-palindromic-substring/discuss/3098/My-simple-c%2B%2B-solution
+     * https://leetcode.com/problems/longest-palindromic-substring/discuss/2923/Simple-C%2B%2B-solution-(8ms-13-lines)
+     * https://leetcode.com/problems/longest-palindromic-substring/discuss/147548/Direct-c%2B%2B-DP
+     * https://leetcode.com/problems/longest-palindromic-substring/discuss/2967/22-line-C%2B%2B-Manacheru2019s-Algorithm-O(n)-Solution
+     */
 }
--- src/OJ0001_0010/Longest_Palindromic_Substring2.java
+++ src/OJ0001_0010/Longest_Palindromic_Substring2.java
@@ -1,64 +1,139 @@
 package OJ0001_0010;
 
-/*
- * http://www.csie.ntnu.edu.tw/~u91029/Palindrome.html
- * 
- * Use Manacher's Algorithm 
- * Last = L = j ; RightMost = R = j+z(j)-1 ; Gusfield's Algorithm
- * nL = n = z[i]
- * 
- */
-
 import java.util.Scanner;
 
 public class Longest_Palindromic_Substring2 {
-	private static int[] z;
-	private char[] s;
+	/*
+	 * http://www.csie.ntnu.edu.tw/~u91029/Palindrome.html
+	 * 
+	 * Use Manacher's Algorithm 
+	 * Last = L = j ; RightMost = R = j+z(j)-1 ; Gusfield's Algorithm
+	 * nL = n = z[i]
+	 * length of palindrome that we use in the algorithm = z[i]-1
+	 * 
+	 */
+	public class LPS1 {
+		private int[] z1;
+		private char[] s1;      // Insert another character which is not in the string
+		
+		public LPS1(char[] line) {
+			int n = line.length;
+			z1 = new int[2 * n + 1];
+			z1[0] = 1;
+			z1[2 * n] = 1;
+			s1 = new char[2 * n + 1];
+			for (int i = 0; i < 2 * n + 1; i++) {
+				if (i % 2 == 0)
+					s1[i] = '.';
+				else
+					s1[i] = line[i / 2];
+			}
+		}
+		
+		private int match(int a, int b) {
+			int i = 0;
+			int n = s1.length;
+			while (a-i >= 0 && b+i < n && s1[a-i] == s1[b+i])
+				i++;
+			return i;
+		}
+		
+		void find(int n) {
+			int Last = 0;
+			int RightMost = 0;
+			for (int i = 1; i < 2 * n; i++) {
+				int ii = Last - (i - Last);
+				int nL = RightMost + 1 - i;
+				
+				if (i > RightMost) {
+					z1[i] = match(i, i);
+					Last = i;
+					RightMost = i + z1[i] - 1;
+				}
+				else if (z1[ii] == nL) {
+					z1[i] = nL + match(i - nL, i + nL);
+					Last = i;
+					RightMost = i + z1[i] - 1;
+				}
+				else {
+					z1[i] = Math.min(z1[ii], nL);
+				}
+			}
+		}
+		
+		int longestPosition() {
+			int maxPosition = 0;
+			for (int i = 1; i < z1.length; i++) {
+				if (z1[i] > z1[maxPosition])
+					maxPosition = i;
+			}
+			return maxPosition;
+		}
+		
+		void printLongest(int p) {
+			int n = z1[p];
+			for (int i = 0; i < 2 * n - 1; i++) {
+				if ( i % 2 == 1)
+					System.out.print(s1[p - n + 1 + i]);
+			}
+		}
+	}
+	
+	/*
+	 * http://www.csie.ntnu.edu.tw/~u91029/Palindrome.html
+	 * 
+	 * Use Manacher's Algorithm
+	 * Last = L = j ; RightMost = R = j+z(j)-1 ; Gusfield's Algorithm
+	 * nL = n = z[i]
+	 * 
+	 */
+	private static int[] z2;
+	private char[] s2;
 	public Longest_Palindromic_Substring2(char[] charline) {
 		int n = charline.length;
-		z = new int[2 * n + 1];
+		z2 = new int[2 * n + 1];
 		// z[0] = 1;            We don't need to calculate it.
 		// z[2 * n] = 1;        We don't need to calculate it.
-		s = new char[2 * n + 1];
+		s2 = new char[2 * n + 1];
 		for (int i = 0; i < 2 * n + 1; i++) {
 			if (i % 2 == 0)
-				s[i] = '.';
+				s2[i] = '.';
 			else
-				s[i] = charline[i / 2];
+				s2[i] = charline[i / 2];
 		}
 	}
 	
 	String find2() {
 		int Last = 0;
 		int RightMost = 0;
-		int n = z.length;
+		int n = z2.length;
 		for (int i = 1; i < n-1; i++) {
 			if (RightMost > i)
-				z[i] = Math.min(z[2 * Last - i], RightMost - i);
+				z2[i] = Math.min(z2[2 * Last - i], RightMost - i);
 			else
-				z[i] = 1;
+				z2[i] = 1;
 			
-			while (i - z[i] >= 0 && i + z[i] < n && s[i - z[i]] == s[i + z[i]])
-				z[i]++;
-		    if (i + z[i] > RightMost) {		    	
+			while (i - z2[i] >= 0 && i + z2[i] < n && s2[i - z2[i]] == s2[i + z2[i]])
+				z2[i]++;
+		    if (i + z2[i] > RightMost) {		    	
 		    	Last = i;
-		    	RightMost = i + z[i];
+		    	RightMost = i + z2[i];
 		    }
 		}
 		
 		String str = ""; 
-		int length = z[longestPosition2()];
+		int length = z2[longestPosition2()];
 		for (int i = 0; i < 2 * length - 1; i++) {
 			if (i % 2 == 1)
-				str += s[longestPosition2() + 1 - length + i];
+				str += s2[longestPosition2() + 1 - length + i];
 		}
 		return str;
 	}
 	
 	int longestPosition2() {
 		int maxPosition = 0;
-		for (int i = 1; i < z.length; i++) {
-			if (z[i] > z[maxPosition])
+		for (int i = 1; i < z2.length; i++) {
+			if (z2[i] > z2[maxPosition])
 				maxPosition = i;
 		}
 		return maxPosition;
@@ -72,7 +147,7 @@ public class Longest_Palindromic_Substring2 {
 		char[] ch = line.toCharArray();
 		Longest_Palindromic_Substring2 lps2 = new Longest_Palindromic_Substring2(ch);
 		System.out.println(lps2.find2());
-		int longest = (z[lps2.longestPosition2()] - 1) / 2;
+		int longest = (z2[lps2.longestPosition2()] - 1) / 2;
 		System.out.println("length of Longest Palindromic Substring : " + longest);
 		
 		keyboard.close();
