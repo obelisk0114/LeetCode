9bc29d687c89edcd1a87fd2e12bb61c579e79aaf
obelisk0114 <obelisk0114@yahoo.com.tw>
2020-01-04 20:57:33 -0800

    Update OJ26
    Add new method

--- src/OJ0021_0030/Remove_Duplicates_from_Sorted_Array.java
+++ src/OJ0021_0030/Remove_Duplicates_from_Sorted_Array.java
@@ -1,8 +1,17 @@
 package OJ0021_0030;
 
 public class Remove_Duplicates_from_Sorted_Array {
-	// https://discuss.leetcode.com/topic/29252/simple-1ms-java-solution
+	// https://leetcode.com/problems/remove-duplicates-from-sorted-array-ii/discuss/27987/Short-and-Simple-Java-solution-(easy-to-understand)
 	public int removeDuplicates(int[] nums) {
+        int i = 0;
+		for (int n : nums)
+			if (i < 1 || n > nums[i - 1])
+				nums[i++] = n;
+		return i;
+    }
+	
+	// https://discuss.leetcode.com/topic/29252/simple-1ms-java-solution
+	public int removeDuplicates3(int[] nums) {
 		int dupes = 0;
 		for (int i = 1; i < nums.length; i++) {
 			if (nums[i] == nums[i - 1])
